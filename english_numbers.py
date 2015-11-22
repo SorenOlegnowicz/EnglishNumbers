4bfa45a2984e3597b8501c4c21ac823133d41b8a
SorenOlegnowicz <soren.olegnowicz@gmail.com>
2015-11-22 09:15:04 -0500

    naming function complete, some slight refactoring. There are still some bugs

--- english_numbers.py
+++ english_numbers.py
@@ -1,26 +1,56 @@
 import re
 
 
+master_list = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+               'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
+               'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen',
+               'nineteen', 'twenty', 'thirty', 'fourty', 'fifty', 'sixty',
+               'seventy', 'eighty', 'ninety', 'hundred']
+
+
 def naming_function(number):
-    pass
+    name_string = ""
+    number = number[::-1].lstrip('0')
+    if len(number) is 1:
+        name_string += master_list[int(number[0])]
+    elif len(number) is 2:
+        if int(number[0]) is 1 or 0:
+            name_string += master_list[int(number[:2])]
+        else:
+            name_string += master_list[int(number[0]) + 18] + ' '
+            name_string += master_list[int(number[1])]
+    elif len(number) is 3:
+        name_string += master_list[int(number[0])] + ' ' + master_list[-1] + ' '
+        if int(number[1]) is 1 or 0:
+            name_string += master_list[int(number[1:])]
+        else:
+            name_string += master_list[int(number[1]) + 18] + ' '
+            name_string += master_list[int(number[2])]
+
+    return name_string.replace(" zero", "")
 
 
 def english_numbers(number, function):
     english_word = []
     # Reverses a list in which each element is 3 digits or less
-    list_of_reversed_number_strings = [x[::-1] for x in re.findall('...?|.', str(number))]
+    number = number[::-1]
+    list_of_reversed_number_strings = re.findall('...?|.', str(number))
+
+    print(list_of_reversed_number_strings)
     # Applies
     for e, i in enumerate(list_of_reversed_number_strings):
-        english_word.insert(function(i), 0)
+        print('i: {}'.format(i))
+        english_word.insert(0, function(i))
         if e is 1:
-            enlgish_word.insert('thousand', 0)
+            print(english_word)
+            english_word.insert(0, 'thousand')
         elif e is 2:
-            enlgish_word.insert('million', 0)
+            english_word.insert(0, 'million')
         elif e is 3:
-            enlgish_word.insert('billion', 0)
+            english_word.insert(0, 'billion')
         elif e is 4:
-            enlgish_word.insert('trillion', 0)
+            english_word.insert(0, 'trillion')
 
-    return enlgish_word.join(' ')
+    return ' '.join(english_word)
 
-print(english_numbers(5001235121))
+print(english_numbers('1991999', naming_function))
